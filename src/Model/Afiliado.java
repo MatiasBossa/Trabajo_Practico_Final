2480588261fc36777aa233085039184187942e7f
MatiasBossa <matubossa@gmail.com>
2020-06-02 12:07:57 -0300

    Modelo Base para trabajar

---
+++ src/Model/Afiliado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.List;
+
+/**
+ *
+ * @author matia
+ */
+public class Afiliado {
+    
+    private int id;
+    private String nombre;
+    private String apellido;
+    private long dni;
+    private List<Orden> movimientos;
+    
+    
+}
---
+++ src/Model/Prestador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.List;
+
+/**
+ *
+ * @author matia
+ */
+public class Prestador extends Afiliado {
+    
+    private int id;
+    private String especialidad;
+    List<Horario> horarios;
+    
+    
+    
+}
