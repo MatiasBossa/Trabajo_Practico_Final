8930d575121d9e03f64b1cefe1e3f713c3262da6
MatiasBossa <matubossa@gmail.com>
2020-06-06 11:52:26 -0300

    Se completo clase EspecialidadData

--- src/Controller/EspecialidadData.java
+++ src/Controller/EspecialidadData.java
@@ -17,9 +17,10 @@ public class EspecialidadData {
     private Connection con = null;
     private Conexion conexion;
     
-    private final String SQL_INSERT = "INSERT INTO especialidad(titulo) VALUES(?)";
+    
     
     public EspecialidadData(Conexion conexion) {
+        
         try {
             this.conexion=conexion;
             con = conexion.getConexion();
@@ -29,15 +30,73 @@ public class EspecialidadData {
     }
     
     public void guardarEspecialidad(Especialidad esp){
+        String SQL_INSERT = "INSERT INTO especialidad(titulo) VALUES(?)";
         try{
             PreparedStatement ps  = con.prepareStatement(SQL_INSERT);
             ps.setString(1, esp.getTitulo());
+            
+            ps.executeUpdate();
+            
+            ps.close();
         }catch(SQLException e){
             System.out.println("ERROR al guardar la Especialidad");
             e.printStackTrace();
         }
     }
     
+    public Especialidad buscarEspecialidad(int id){
+        String SQL_SELECT = "SELECT * FROM especialidad WHERE idEspecialidad = ?";
+        ResultSet rs;
+        Especialidad esp = null;
+        try{
+            PreparedStatement ps = con.prepareStatement(SQL_SELECT);
+            ps.setInt(1, id);
+            
+             rs = ps.executeQuery();
+             
+             if(rs.next()){
+                 esp = new Especialidad(rs.getInt(1), rs.getString(2));
+             }
+             
+             rs.close();
+             ps.close();
+        }catch(SQLException e){
+            System.out.println("ERROR al encontrar la Especialidad");
+            
+        }
+        
+        return esp;
+    }
     
+    public void borrarEspecialidad(int id){
+        String SQL_DELETE = "DELETE FROM especialidad WHERE idEspecialidad = ?";
+        try{
+            PreparedStatement ps = con.prepareStatement(SQL_DELETE);
+            ps.setInt(1, id);
+            
+            ps.executeUpdate();
+            
+            ps.close();
+        }catch(SQLException e){
+            System.out.println("ERROR al eliminar la especialidad");
+            e.printStackTrace();
+        }
+    }
+    
+    public void modificarEspecialidad(Especialidad esp){
+        String SQL_UPDATE = "UPDATE especialidad SET titulo = ? WHERE idEspecialidad = ?";
+        try{
+            PreparedStatement ps = con.prepareStatement(SQL_UPDATE);
+            ps.setString(1, esp.getTitulo());
+            ps.setInt(2, esp.getIdEspecialidad());
+            
+            ps.executeUpdate();
+            
+            ps.close();
+        }catch(SQLException e){
+            System.out.println("ERROR al actualizar la especialidad");
+            e.printStackTrace();
+        }
+    }
     
 }
