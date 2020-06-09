b92ab61f2040da9bd3746fe03b9fa0c6c548e7eb
HP <HP@miguel-PC>
2020-06-08 23:14:10 -0300

    Subiendo nuevamente clse CtrlOrden. Subiendo el UML del proyecto

--- src/View/Inicio.java
+++ src/View/Inicio.java
@@ -5,7 +5,7 @@
  */
 package View;
 
-import Controller.ctrlOrden;
+import Controller.CtrlOrden;
 import Model.Data.Conexion;
 import Model.Data.OrdenData;
 import Model.Entities.Orden;
@@ -126,7 +126,7 @@ public class Inicio extends javax.swing.JFrame {
         OrdenData modD = new OrdenData();
         frmABMorden frm = new frmABMorden();
         
-        ctrlOrden ctrl = new ctrlOrden(modE, modD, frm);
+        CtrlOrden ctrl = new CtrlOrden(modE, modD, frm);
         ctrl.iniciar();
         frm.setVisible(true);
     }//GEN-LAST:event_btn_ordenActionPerformed
